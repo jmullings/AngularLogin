d45f0bdfc1bb0b8995dda22c2733c2de898ff50c
BlackCellReact <jason.mullings@calatacorp.com>
2018-09-12 19:04:09 +0800

    Angular start script

--- server.js
+++ server.js
@@ -7,13 +7,11 @@ const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/angularlog'));
+app.use(express.static(__dirname + '/AngularLogin'));
 
 app.get('/*', function(req,res) {
 
-    res.sendFile(path.join(__dirname+'/dist/angularlog/index.html'));
+    res.sendFile(path.join(__dirname+'/src/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
